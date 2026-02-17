a01458a12d5def913b3526560d5d5ba762dc3cd8
sharmaRishit12 <rs1943@srmist.edu.in>
2026-02-17 09:38:36 +0530

    Added uc2 print oops with *

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,5 +1,6 @@
 public class OopsBanner{
     public static void main(String[] args){
+        // UC2: print OOPS with *
         System.out.println("  ****   " + " " + "  ****   " + " " + " *****   " + " " + "  ***** ");
         System.out.println(" **  **  " + " " + " **  **  " + " " + " **   ** " + " " + " **     ");
         System.out.println("**    ** " + " " + "**    ** " + " " + " **   ** " + " " + " **     ");
