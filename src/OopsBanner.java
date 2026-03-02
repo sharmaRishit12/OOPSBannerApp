6736b7e1cbbf4856dda2362c1b44184423457f26
sharmaRishit12 <rs1943@srmist.edu.in>
2026-03-02 08:33:06 +0530

    added UC3

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -8,5 +8,71 @@ public class OopsBanner{
         System.out.println("**    ** " + " " + "**    ** " + " " + " **      " + " " + "      **");
         System.out.println(" **  **  " + " " + " **  **  " + " " + " **      " + " " + " **   **");
         System.out.println("  ****   " + " " + "  ****   " + " " + " **      " + " " + "  ***** ");
+    }public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        printBanner();
     }
+
+    public static void printBanner() {
+
+        String line1 = String.join(" ",
+                "  ****   ",
+                "  ****   ",
+                " *****   ",
+                "  ***** "
+        );
+
+        String line2 = String.join(" ",
+                " **  **  ",
+                " **  **  ",
+                " **   ** ",
+                " **     "
+        );
+
+        String line3 = String.join(" ",
+                "**    ** ",
+                "**    ** ",
+                " **   ** ",
+                " **     "
+        );
+
+        String line4 = String.join(" ",
+                "**    ** ",
+                "**    ** ",
+                " *****   ",
+                "  ***** "
+        );
+
+        String line5 = String.join(" ",
+                "**    ** ",
+                "**    ** ",
+                " **      ",
+                "      **"
+        );
+
+        String line6 = String.join(" ",
+                " **  **  ",
+                " **  **  ",
+                " **      ",
+                " **   **"
+        );
+
+        String line7 = String.join(" ",
+                "  ****   ",
+                "  ****   ",
+                " **      ",
+                "  ***** "
+        );
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
 }
